e78574dc83b1d6e8d05305baf7baedc2feb7cdec
phanther1 <144461089+phanther1@users.noreply.github.com>
2023-12-05 10:13:55 -0500

    hhd

--- api-proj/js/main.js
+++ api-proj/js/main.js
@@ -1,4 +1,4 @@
-const URL = `https://api.dictionaryapi.dev/api/v2/entries/en/`;
+/* const URL = `https://animechan.xyz/api/random`;
 
 async function getData(URL) {
 
@@ -21,4 +21,43 @@ async function getData(URL) {
     }
 }
 
-getData(URL);
+getData(URL); */
+
+
+const picture = "https://picsum.photos/200"
+async function getImg(picture) {
+
+    try {
+
+        const response = await fetch(picture);
+        if (response.status != 200) {
+            throw new Error(response.statusText);
+
+        }
+
+        const data = await response.json();
+        document.querySelector(".images").textContent = data.content;
+        console.log(data.content);
+        console.log("working");
+
+    } catch (error) {
+        console.log("oopai, error meh");
+        document.querySelector("h1").textContent = "Your subscription go bye bye";
+    }
+}
+
+getImg(picture);
+
+/* function insertCard() {
+    document.querySelector(".cards").insertAdjacentHTML(
+
+        
+
+      "beforeend",
+      `<div class="card">
+        <h2 class = "text">"ahahahah"</h2>
+        <img src= alt="" class="card-img">
+        <h2 class = "info"> </h2>
+      </div>`
+    );
+  } */
