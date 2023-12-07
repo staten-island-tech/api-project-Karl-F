8956f03c67505489f0ee8c6ea242f00195eb998c
Karl F <144461089+phanther1@users.noreply.github.com>
2023-12-06 19:21:43 -0500

    dfdsf

--- api-proj/js/main.js
+++ api-proj/js/main.js
@@ -1,65 +1,62 @@
-const URL = `https://picsum.photos/200`;
+/* const URL = "http://api.quotable.io/random";
 
 async function getData(URL) {
 
     try {
+        const response = await fetch(URL);
+        const data = await response.json();
+        document.querySelector("#cards").textContent = data.content;
+        console.log(response);
+        if (response.status != 200) {
+            throw new Error(response.statusText);
+        }
 
+        
+        console.log(data);
+        let test = document.querySelector("#cards");
+        test.forEach((picturelink) => test.insertAdjacentHTML( "beforeend",
+        `
+        <div class="card">
+        <img src="${picturelink}" alt=""><br>
+        <button class="btn">Click</button>
+        </div>
+        `
+        ))}
+
+     catch (error) {
+        console.log("oopai, error meh");
+        document.querySelector("h1").textContent = "Your subscription go bye bye";
+    }
+}
+
+getData(URL);  */
+
+const URL = `https://api.imgflip.com/get_memes`;
+
+
+async function getData(URL) {
+    try {
         const response = await fetch(URL);
         if (response.status != 200) {
             throw new Error(response.statusText);
-
         }
-
         const data = await response.json();
-        document.querySelector("h1").textContent = data.content;
-        console.log(data.content);
-        console.log("working");
+        document.querySelector(".cards").textContent = data.memes;
+        console.log(data.data.memes); 
+        console.log(data);
+        /* test.forEach((test) => test.insertAdjacentHTML( "beforeend",
+        `
+        <div class="card">
+        <img src="${url}" alt=""><br>
+        <button class="btn">Click</button>
+        </div>
+        `
+        )) */
 
     } catch (error) {
         console.log("oopai, error meh");
-        document.querySelector("h1").textContent = "Your subscription go bye bye";
+        document.querySelector(".cards").textContent = "Your subscription go bye bye";
     }
 }
 
-getData(URL); 
-
-
-/* const picture = "https://picsum.photos/200"
-async function getImg(picture) {
-
-    try {
-
-        const response = await fetch(picture);
-        if (response.status != 200) {
-            throw new Error(response.statusText);
-
-        }
-
-        const data = await response.json();
-        document.querySelector(".images").textContent = data.content;
-        console.log(data.content);
-        console.log("working");
-
-    } catch (error) {
-        console.log("oopai, error meh");
-        document.querySelector("h1").textContent = "Your subscription go bye bye";
-    }
-}
-
-getImg(picture); */
-
-
-
-/* function insertCard() {
-    document.querySelector(".cards").insertAdjacentHTML(
-
-        
-
-      "beforeend",
-      `<div class="card">
-        <h2 class = "text">"ahahahah"</h2>
-        <img src= alt="" class="card-img">
-        <h2 class = "info"> </h2>
-      </div>`
-    );
-  } */
+getData(URL);
