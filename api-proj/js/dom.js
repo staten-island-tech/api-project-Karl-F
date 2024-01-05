616f0ead97886ebed800b20960884adc48d9c568
Karl F <144461089+phanther1@users.noreply.github.com>
2024-01-04 21:32:49 -0500

    whats dialog

--- api-proj/js/dom.js
+++ api-proj/js/dom.js
@@ -1,6 +1,10 @@
 const DOMSelectors = {
     container: document.querySelector(".cards"),
-    form: document.querySelector("#form"),
+    top25: document.querySelector("#top25Btn"),
+    top50: document.querySelector("#top50Btn"),
+    top75: document.querySelector("#top75Btn"),
+    top100: document.querySelector("#top100Btn"),
+    random: document.querySelector("#randomBtn"),
 }
 
 export {DOMSelectors}
--- api-proj/js/main.js
+++ api-proj/js/main.js
@@ -1,76 +1,110 @@
-/* const URL = "http://api.quotable.io/random";
-
-async function getData(URL) {
-
-    try {
-        const response = await fetch(URL);
-        const data = await response.json();
-        document.querySelector("#cards").textContent = data.content;
-        console.log(response);
-        if (response.status != 200) {
-            throw new Error(response.statusText);
-        }
-
-        
-        console.log(data);
-        let test = document.querySelector("#cards");
-        test.forEach((picturelink) => test.insertAdjacentHTML( "beforeend",
-        `
-        <div class="card">
-        <img src="${picturelink}" alt=""><br>
-        <button class="btn">Click</button>
-        </div>
-        `
-        ))}
-
-     catch (error) {
-        console.log("oopai, error meh");
-        document.querySelector("h1").textContent = "Your subscription go bye bye";
-    }
-}
-
-getData(URL);  */
-
-
-import "../styles/style.css"
+import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-const URL = `https://api.imgflip.com/get_memes`;
+const MEMES_URL = `https://api.imgflip.com/get_memes`;
+const CAPTION_URL = `https://api.imgflip.com/caption_image`;
 
 async function getData(URL) {
     try {
         const response = await fetch(URL);
-        if (response.status != 200) {
+        if (response.status !== 200) {
             throw new Error(response.statusText);
         }
         const grab = await response.json();
         const arrData = grab.data;
+        console.log(grab.data);
 
-        console.log(grab);
-
-            function insertmeme() {
-            arrData.memes.forEach((meme) => document.querySelector(".cards").insertAdjacentHTML("beforeend",
-            `
-            <div class="card">
-            <h2 class = "description">${meme.name}</h2>
-            <img class = "image" src="${meme.url}" alt="${meme.name}"><br>
-            </div>
-            `
-            ));
+        function insertMeme(top) {
+            const topMemes = arrData.memes.slice(0, top);
+            document.querySelector(".cards").innerHTML = "";
+            topMemes.forEach((meme) => {
+                document.querySelector(".cards").insertAdjacentHTML("beforeend",
+                    `
+                    <div class="card">
+                        <h2 class="description">${meme.name}</h2>
+                        <img class="image" src="${meme.url}" alt="${meme.name}" data-id="${meme.id}">
+                        <button class="caption-button" data-id="${meme.id}">Caption It</button>
+                    </div>
+                    `
+                );
+            });
         }
-        insertmeme();
-/* 
-        const memeNum = arrData.memes.filter((meme) => meme.id < 200000); 
-        const countMeme = memeNum.id.length;
-        console.log (`meme set: ${countMeme}`);
- */
+        insertMeme(100);
+
+        function randomMemes() {
+            const randomMemes = getRandomMemes(arrData.memes, 20);
+            document.querySelector(".cards").innerHTML = "";
+            randomMemes.forEach((meme) => {
+                document.querySelector(".cards").insertAdjacentHTML("beforeend",
+                    `
+                    <div class="card">
+                        <h2 class="description">${meme.name}</h2>
+                        <img class="image" src="${meme.url}" alt="${meme.name}" data-id="${meme.id}">
+                        <button class="caption-button" data-id="${meme.id}">Caption It</button>
+                    </div>
+                    `
+                );
+            });
+        }
+        function getRandomMemes(memes, num) {
+            const randomMeme = memes.slice(0).sort(() => 0.5 - Math.random()); //subtract random num from 0.5 btwn 0-1.
+            return randomMeme.slice(0, num);
+        }
+
+        DOMSelectors.top25.addEventListener("click", () => insertMeme(25));
+        DOMSelectors.top50.addEventListener("click", () => insertMeme(50));
+        DOMSelectors.top75.addEventListener("click", () => insertMeme(75));
+        DOMSelectors.top100.addEventListener("click", () => insertMeme(100));
+        DOMSelectors.random.addEventListener("click", randomMemes); 
+
+        document.querySelector(".cards").addEventListener("click", async (event) => { //adds event to all caption it button, I think
+            const clickedButton = event.target.closest(".caption-button");
+            if (clickedButton) {
+                const memeId = clickedButton.getAttribute("data-id");
+                const captions = await addCaptions();
+                if (captions) {
+                await captionImage(memeId, captions);
+            }}
+        });
+
     } catch (error) {
-        console.log("oopai, error meh");
+        console.log("Oopai error meh:", error);
         document.querySelector(".cards").textContent = "Your subscription go bye bye";
-    };
-};
-getData(URL);
+    }
+}
+
+//change to smth like dialog
+async function addCaptions() {
+    const text1 = prompt("Enter caption for first text");
+    const text2 = prompt("Enter caption for second text");
+    return [text1, text2];
+}
+
+async function captionImage(memeId, captions) {
+    try {
+        const response = await fetch(CAPTION_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `template_id=${memeId}&username=phanther56&password=Apple123&text0=${captions[0]}&text1=${captions[1]}`,
+        });
+
+        if (response.status !== 200) {
+            throw new Error(response.statusText);
+        }
+
+        const result = await response.json();
+        console.log("Caption results", result);
+
+        if (result.success) {
+            console.log("Newly created image URL:", result.data.url);
+        }
 
 
-
+    } catch (error) {
+        console.log("Captioning image broke meh:", error);
+    }
+}
+getData(MEMES_URL);
 
