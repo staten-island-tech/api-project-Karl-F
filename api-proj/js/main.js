a5a19e3bd1a85cf66b7f0c2d04611e7011ecacd3
Karl F <144461089+phanther1@users.noreply.github.com>
2024-01-06 15:11:16 -0500

    f

--- api-proj/js/main.js
+++ api-proj/js/main.js
@@ -1,8 +1,8 @@
 import "../styles/style.css";
 import { DOMSelectors } from "./dom";
 
-const MEMES_URL = `https://api.imgflip.com/get_memes`;
-const CAPTION_URL = `https://api.imgflip.com/caption_image`;
+const memeURL = `https://api.imgflip.com/get_memes`;
+const captionURL = `https://api.imgflip.com/caption_image`;
 
 async function getData(URL) {
     try {
@@ -17,47 +17,59 @@ async function getData(URL) {
         function insertMeme(top) {
             const topMemes = arrData.memes.slice(0, top);
             document.querySelector(".cards").innerHTML = "";
-            topMemes.forEach((meme) => {
-                document.querySelector(".cards").insertAdjacentHTML("beforeend",
-                    `
-                    <div class="card">
-                        <h2 class="description">${meme.name}</h2>
-                        <img class="image" src="${meme.url}" alt="${meme.name}" data-id="${meme.id}"> <br>
-                        <button class="caption-button" data-id="${meme.id}">Caption It</button>
-                    </div>
-                    `
-                );
-            });
+            topMemes.forEach((meme) => document.querySelector(".cards").insertAdjacentHTML("beforeend",
+                `<div class="card">
+                    <h3 class="description">${meme.name}</h3>
+                    <img class="image" src="${meme.url}" alt="${meme.name}" data-id="${meme.id}"> <br>
+                    <button class="caption-button" data-id="${meme.id}">Caption It</button>
+                </div>`
+            ));
         }
-        insertMeme(100);
+        insertMeme(100); 
 
         function randomMemes() {
             const randomMemes = getRandomMemes(arrData.memes, 20);
             document.querySelector(".cards").innerHTML = "";
-            randomMemes.forEach((meme) => {
-                document.querySelector(".cards").insertAdjacentHTML("beforeend",
-                    `
-                    <div class="card">
-                        <h2 class="description">${meme.name}</h2>
-                        <img class="image" src="${meme.url}" alt="${meme.name}" data-id="${meme.id}"> <br>
-                        <button class="caption-button" data-id="${meme.id}">Caption It</button>
-                    </div>
-                    `
-                );
-            });
+            randomMemes.forEach((meme) => document.querySelector(".cards").insertAdjacentHTML("beforeend",
+                `<div class="card">
+                    <h3 class="description">${meme.name}</h3>
+                    <img class="image" src="${meme.url}" alt="${meme.name}" data-id="${meme.id}"> <br>
+                    <button class="caption-button" data-id="${meme.id}">Caption It</button>
+                </div>`
+            ));
         }
         function getRandomMemes(memes, num) {
             const randomMeme = memes.slice(0).sort(() => 0.5 - Math.random()); //subtract random num from 0.5 btwn 0-1.
             return randomMeme.slice(0, num);
         }
 
-        DOMSelectors.top25.addEventListener("click", () => insertMeme(25));
-        DOMSelectors.top50.addEventListener("click", () => insertMeme(50));
-        DOMSelectors.top75.addEventListener("click", () => insertMeme(75));
-        DOMSelectors.top100.addEventListener("click", () => insertMeme(100));
-        DOMSelectors.random.addEventListener("click", randomMemes); 
+        function highlight(button) {
+            const buttons = [DOMSelectors.top25, DOMSelectors.top50, DOMSelectors.top75, DOMSelectors.top100, DOMSelectors.random];
+            buttons.forEach((btn) => btn.classList.remove('clicked'));
+            button.classList.add('clicked');
+        }
+        DOMSelectors.top25.addEventListener("click", () => {
+            insertMeme(25);
+            highlight(DOMSelectors.top25);
+        });
+        DOMSelectors.top50.addEventListener("click", () => {
+            insertMeme(50);
+            highlight(DOMSelectors.top50);
+        });
+        DOMSelectors.top75.addEventListener("click", () => {
+            insertMeme(75);
+            highlight(DOMSelectors.top75);
+        });
+        DOMSelectors.top100.addEventListener("click", () => {
+            insertMeme(100);
+            highlight(DOMSelectors.top100);
+        });
+        DOMSelectors.random.addEventListener("click", () => {
+            randomMemes();
+            highlight(DOMSelectors.random);
+        });
 
-        document.querySelector(".cards").addEventListener("click", async (event) => { //adds event to all caption it button, I think
+        document.querySelector(".cards").addEventListener("click", async (event) => { //adds event to all caption it buttons, I think
             const clickedButton = event.target.closest(".caption-button");
             if (clickedButton) {
                 const memeId = clickedButton.getAttribute("data-id");
@@ -73,16 +85,18 @@ async function getData(URL) {
     }
 }
 
-//change to smth like dialog
+//Use prompt box
 async function addCaptions() {
     const text1 = prompt("Enter caption for first text");
     const text2 = prompt("Enter caption for second text");
     return [text1, text2];
 }
 
+//want to use dialog box, pls help
+
 async function captionImage(memeId, captions) {
     try {
-        const response = await fetch(CAPTION_URL, {
+        const response = await fetch(captionURL, {
             method: "POST",
             headers: {
                 "Content-Type": "application/x-www-form-urlencoded",
@@ -99,12 +113,11 @@ async function captionImage(memeId, captions) {
 
         if (result.success) {
             console.log("Newly created image URL:", result.data.url);
+            window.open(result.data.url, '_blank');
         }
-
 
     } catch (error) {
         console.log("Captioning image broke meh:", error);
     }
 }
-getData(MEMES_URL);
-
+getData(memeURL);
